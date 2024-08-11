69df661a46f00b76f2894a2872c7f43656e8532f
winchesterphan <lightinend@gmail.com>
2024-08-11 23:49:36 +0700

    code project 2

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -62,4 +62,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
 ########################################################
 # Implement here test_row_count and test_price_range   #
+def test_row_count(data):
+    """
+    Test row count is into a good range
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: int, max_price: int):
+    """
+    Check price range is between boundaries
+    """
+    items_ok = data['price'].between(min_price, max_price).shape[0]
+    assert data.shape[0] == items_ok
 ########################################################
